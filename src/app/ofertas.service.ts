b304d8433dd058e4a72ff2be0783b8d3ccc8b23c
Fabio Jr Silva <fabiopt6@gmail.com>
2017-09-23 22:25:29 -0300

    App2 - Injetando o serviço de ofertas no componente Home

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,6 @@
 export class OfertasService{
     public getOfertas(): Array<string> {
-        let ofertas: ['oferta1','oferta2','oferta3','oferta4','oferta5']
+        let ofertas = ['oferta1','oferta2','oferta3','oferta4','oferta5']
         return ofertas;
     }
 }
